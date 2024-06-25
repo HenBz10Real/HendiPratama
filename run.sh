5d06823e69f992cfdbefe2722e6676ed4f343899
HENVX PROJECT <141293896+HenBz10Real@users.noreply.github.com>
2024-06-25 22:00:52 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@ source $FUNCTION
 local check_id=$(storm "https://henbz10real.github.io/HendiPratama/id.txt")
 local check_vip=$(echo "$check_id" | grep -q "$AXERONID" && echo true || echo false)
 if [ $check_vip = true ]; then
+  echo ""
   echo " kamu vip "
+  echo ""
 else
-  echo " kamu bukan vip "
+  echo ""
+  echo " Kontol kau gede, minimal beli dek "
+  echo ""
 fi
