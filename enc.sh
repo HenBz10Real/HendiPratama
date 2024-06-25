a6c340ad743a418a9b65ed4a4a54d432950abb1c
HENVX PROJECT <141293896+HenBz10Real@users.noreply.github.com>
2024-06-25 21:37:25 +0700

    Update enc.sh

--- enc.sh
+++
@@ -1,8 +0,0 @@
-source $FUNCTION
-local check_id=$(storm "https://henbz10real.github.io/HendiPratama/id.txt")
-local check_vip=$(echo "$check_id" | grep -q "$AXERONID" && echo true || echo false)
-if [ $check_vip = true ]; then
-  echo " kamu vip "
-else
-  echo " kamu bukan vip "
-fi
