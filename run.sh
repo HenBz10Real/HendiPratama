9de65941d2ad5a4e07bcd63dd91b6e46e23f76f1
HENVX PROJECT <141293896+HenBz10Real@users.noreply.github.com>
2024-06-26 21:45:24 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 source $FUNCTION
-local check_id=$(storm "https://henbz10real.github.io/HendiPratama/id.txt")
+local check_id=$(storm "r17rYI0tYD6Cp9fQN5zvaVntdMysT5erOIfuNZlrN8mJXMlQXcHtdW0PN1yC2cbCeI4uR==")
 local check_vip=$(echo "$check_id" | grep -q "$AXERONID" && echo true || echo false)
 if [ $check_vip = true ]; then
   echo ""
